9f95d1b8895c6ad8f470c87365d05e14d1584db6
El <el__1997@mail.ru>
2023-01-11 11:02:00 +0300

    if libs fails

--- src/helpers/calcDeclination.js
+++ src/helpers/calcDeclination.js
@@ -2,6 +2,6 @@ const geomagnetism = require('geomagnetism');
 
 export const calcDeclination = (latitude, longitude) => {
     const info = geomagnetism.model().point([latitude, longitude]);
-    return info.decl
+    return info ? info.decl : 0
 }
 
--- src/helpers/handleRedirect.js
+++ src/helpers/handleRedirect.js
@@ -1,20 +1,25 @@
 const REDIRECT_URL = process.env.REACT_APP_REDIRECT_URL;
 
 const getParams = (search) => {
-    const params = {}
-    const pairs = search.split('?')[1].split('&')
-    for (let i= 0; i < pairs.length; i++) {
-      const pair = pairs[i].split('=')
-      params[pair[0]] = pair[1]
-    }
+  const params = {};
+  const pairs = search.split("?")[1].split("&");
+  for (let i = 0; i < pairs.length; i++) {
+    const pair = pairs[i].split("=");
+    params[pair[0]] = pair[1];
+  }
   return params;
 };
 
 export const handleRedirect = () => {
   const search = window.location.search;
+  if (!REDIRECT_URL) {
+    return;
+  }
   if (search) {
     const coords = getParams(search);
-    window.location.assign(`${REDIRECT_URL}?longitude=${coords.longitude}&latitude=${coords.latitude}&zoom=17&t=m`)
+    window.location.assign(
+      `${REDIRECT_URL}?longitude=${coords.longitude}&latitude=${coords.latitude}&zoom=17&t=m`
+    );
   } else {
     window.location.assign(REDIRECT_URL);
   }
