0edc1cb51c284165380cf13415392f01d8c6187f
El <el__1997@mail.ru>
2023-01-17 21:09:09 +0300

    debug

--- src/components/compass/Compass.js
+++ src/components/compass/Compass.js
@@ -36,7 +36,8 @@ function Compass({
         <img src={arrow} className="compass-arrow" alt="arrow" />
       </div>
       <p className="compass-alert" style={{ color: isQibla ? "green" : "red" }}>
-        {start && message}
+        {/* {start && message} */}
+        {window.location.search}
       </p>
     </div>
   );
