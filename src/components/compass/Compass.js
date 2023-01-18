831011ef8d3519415e2de08452849a98e2281e83
El <el__1997@mail.ru>
2023-01-18 04:28:53 +0300

    debug

--- src/components/compass/Compass.js
+++ src/components/compass/Compass.js
@@ -40,7 +40,7 @@ function Compass({
       </div>
       <p className="compass-alert" style={{ color: isQibla ? "green" : "red" }}>
         {/* {start && message} */}
-  {coords && "Coords:" + coords.latitude + ", " + coords.longitude || err}
+  {"Coords:" + coords && coords.latitude + ", " + coords && coords.longitude || err}
       </p>
     </div>
   );
